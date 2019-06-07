3c1a9cba78d44c7b575750a4b425f8aa85aed2b4
dinoleung <dino@330z.net>
2019-06-07 18:20:14 +0930

    just need mopped

--- bin/server.dart
+++ bin/server.dart
@@ -3,94 +3,29 @@ import 'package:teledart/telegram.dart';
 import 'package:teledart/model.dart';
 
 import 'package:DartWiseBot/variables.dart';
-import 'package:DartWiseBot/functions.dart';
+import 'package:DartWiseBot/responds.dart';
 
 void main() {
   final TeleDart teledart = TeleDart(Telegram(envVars['BOT_TOKEN']), Event());
-
-  teledart.setupWebhook(envVars['HOST_URL'], envVars['BOT_TOKEN'], cert, key,
-      port: int.parse(envVars['BOT_PORT']));
-
-  teledart.start(webhook: true).then((User user) => me = user);
+  final Responds respond = Responds(teledart);
 
   teledart
-      .onCommand('start')
-      .listen((Message message) => teledart.replyMessage(message, startMsg));
-
-  teledart.onCommand('help').listen((Message message) =>
-      teledart.replyMessage(message, helpMsg, parse_mode: 'markdown'));
-
-  teledart.onCommand('about').listen((Message message) => teledart.replyMessage(
-      message, githubMsg,
-      parse_mode: 'markdown', disable_web_page_preview: true));
-
-  teledart
-      .onCommand('flip')
-      .listen((Message message) => teledart.replyMessage(message, coin()));
-
-  teledart
-      .onCommand('roll')
-      .listen((Message message) => teledart.replyMessage(message, die()));
-
-  teledart
-      .onCommand('draw')
-      .listen((Message message) => teledart.replyMessage(message, card()));
-
-  teledart.onCommand('pick').listen((Message message) => teledart.replyMessage(
-      message, pick(getCommandQuery(message), commandHasUsername(message)),
-      parse_mode: 'markdown'));
-
-  teledart.onCommand('learn').listen((Message message) => teledart.replyMessage(
-      message, learn(getCommandQuery(message)),
-      parse_mode: 'markdown', disable_web_page_preview: true));
-
-  teledart.onCommand('suggest').listen((Message message) {
-    String suggestions = getCommandQuery(message);
-
-    if (suggestions.isEmpty)
-      return teledart.replyMessage(message,
-          invalidSuggestions(hasUsername: commandHasUsername(message)));
-
-    teledart.telegram.sendMessage(int.parse(envVars['MYID']),
-        suggestionMsg(message.from.username, suggestions));
-    return teledart.replyMessage(message, validSuggestions);
-  });
-
-  teledart.onInlineQuery().listen((InlineQuery inlineQuery) {
-    List<InlineQueryResult> results = [
-      InlineQueryResultArticle()
-        ..id = 'flip'
-        ..title = '🌝'
-        ..input_message_content = (InputTextMessageContent()
-          ..message_text = coin()
-          ..parse_mode = 'markdown'),
-      InlineQueryResultArticle()
-        ..id = 'roll'
-        ..title = '🎲'
-        ..input_message_content = (InputTextMessageContent()
-          ..message_text = die()
-          ..parse_mode = 'markdown'),
-      InlineQueryResultArticle()
-        ..id = 'draw'
-        ..title = '🃏'
-        ..input_message_content = (InputTextMessageContent()
-          ..message_text = card()
-          ..parse_mode = 'markdown'),
-      InlineQueryResultArticle()
-        ..id = 'pick'
-        ..title = 'Pick from'
-        ..description = '🅰️, 🅱️,...'
-        ..input_message_content = (InputTextMessageContent()
-          ..message_text = pick(inlineQuery.query, false)
-          ..parse_mode = 'markdown'),
-      InlineQueryResultArticle()
-        ..id = 'learn'
-        ..title = 'Ask the wise...'
-        ..description = '🧙'
-        ..input_message_content = (InputTextMessageContent()
-          ..message_text = learn(inlineQuery.query)
-          ..parse_mode = 'markdown'),
-    ];
-    teledart.answerInlineQuery(inlineQuery, results);
-  });
+    ..setupWebhook(
+      envVars['HOST_URL'],
+      envVars['BOT_TOKEN'],
+      cert,
+      key,
+      port: int.parse(envVars['BOT_PORT']),
+    )
+    ..start(webhook: true).then((User user) => me = user)
+    ..onCommand('start').listen(respond.sendStartMessage)
+    ..onCommand('help').listen(respond.sendHelpMessage)
+    ..onCommand('about').listen(respond.sendAboutMessage)
+    ..onCommand('flip').listen(respond.sendCoinMessage)
+    ..onCommand('roll').listen(respond.sendDieMessage)
+    ..onCommand('draw').listen(respond.sendCardMessage)
+    ..onCommand('pick').listen(respond.sendChoiceMessage)
+    ..onCommand('learn').listen(respond.sendWiseMessage)
+    ..onCommand('suggest').listen(respond.sendSuggestMessage)
+    ..onInlineQuery().listen(respond.answerInlineQuery);
 }
---
+++ lib/responds.dart
@@ -0,0 +1,88 @@
+import 'package:teledart/teledart.dart';
+import 'package:teledart/model.dart';
+
+import 'package:DartWiseBot/variables.dart';
+import 'package:DartWiseBot/functions.dart';
+
+class Responds {
+  static TeleDart _teledart;
+
+  Responds(TeleDart teledart) {
+    _teledart = teledart;
+  }
+
+  Future<Message> sendStartMessage(Message message) =>
+      _teledart.replyMessage(message, startMsg);
+
+  Future<Message> sendHelpMessage(Message message) =>
+      _teledart.replyMessage(message, helpMsg, parse_mode: 'markdown');
+
+  Future<Message> sendAboutMessage(Message message) =>
+      _teledart.replyMessage(message, githubMsg,
+          parse_mode: 'markdown', disable_web_page_preview: true);
+
+  Future<Message> sendCoinMessage(Message message) =>
+      _teledart.replyMessage(message, coin());
+
+  Future<Message> sendDieMessage(Message message) =>
+      _teledart.replyMessage(message, die());
+
+  Future<Message> sendCardMessage(Message message) =>
+      _teledart.replyMessage(message, card());
+
+  Future<Message> sendChoiceMessage(Message message) => _teledart.replyMessage(
+      message, pick(getCommandQuery(message), commandHasUsername(message)),
+      parse_mode: 'markdown');
+
+  Future<Message> sendWiseMessage(Message message) =>
+      _teledart.replyMessage(message, learn(getCommandQuery(message)),
+          parse_mode: 'markdown', disable_web_page_preview: true);
+
+  Future<Message> sendSuggestMessage(Message message) {
+    String suggestions = getCommandQuery(message);
+
+    if (suggestions.isEmpty)
+      return _teledart.replyMessage(message,
+          invalidSuggestions(hasUsername: commandHasUsername(message)));
+
+    _teledart.telegram.sendMessage(int.parse(envVars['MYID']),
+        suggestionMsg(message.from.username, suggestions));
+    return _teledart.replyMessage(message, validSuggestions);
+  }
+
+  Future<bool> answerInlineQuery(InlineQuery inlineQuery) =>
+      _teledart.answerInlineQuery(inlineQuery, [
+        InlineQueryResultArticle()
+          ..id = 'flip'
+          ..title = '🌝'
+          ..input_message_content = (InputTextMessageContent()
+            ..message_text = coin()
+            ..parse_mode = 'markdown'),
+        InlineQueryResultArticle()
+          ..id = 'roll'
+          ..title = '🎲'
+          ..input_message_content = (InputTextMessageContent()
+            ..message_text = die()
+            ..parse_mode = 'markdown'),
+        InlineQueryResultArticle()
+          ..id = 'draw'
+          ..title = '🃏'
+          ..input_message_content = (InputTextMessageContent()
+            ..message_text = card()
+            ..parse_mode = 'markdown'),
+        InlineQueryResultArticle()
+          ..id = 'pick'
+          ..title = 'Pick from'
+          ..description = '🅰️, 🅱️,...'
+          ..input_message_content = (InputTextMessageContent()
+            ..message_text = pick(inlineQuery.query, false)
+            ..parse_mode = 'markdown'),
+        InlineQueryResultArticle()
+          ..id = 'learn'
+          ..title = 'Ask the wise...'
+          ..description = '🧙'
+          ..input_message_content = (InputTextMessageContent()
+            ..message_text = learn(inlineQuery.query)
+            ..parse_mode = 'markdown'),
+      ]);
+}
